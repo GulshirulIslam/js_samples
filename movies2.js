890b324218cc0d26165c67cec4f001ae215d9560
Gulshir <gulshir.i@lucideustech.com>
2020-07-25 01:08:21 +0530

    Added JS samples

---
+++ movies2.js
@@ -0,0 +1,62 @@
+/**
+*** Version 2 of movies program - Short & simple 
+**/ 
+
+const movieList = [
+  {
+    title: "Batman",
+    year: 1989,
+    director: "Tim Burton",
+    imdbRating: 7.6
+  },
+  {
+    title: "Batman Returns",
+    year: 1992,
+    director: "Tim Burton",
+    imdbRating: 7.0
+  },
+  {
+    title: "Batman Forever",
+    year: 1995,
+    director: "Joel Schumacher",
+    imdbRating: 5.4
+  },
+  {
+    title: "Batman & Robin",
+    year: 1997,
+    director: "Joel Schumacher",
+    imdbRating: 3.7
+  },
+  {
+    title: "Batman Begins",
+    year: 2005,
+    director: "Christopher Nolan",
+    imdbRating: 8.3
+  },
+  {
+    title: "The Dark Knight",
+    year: 2008,
+    director: "Christopher Nolan",
+    imdbRating: 9.0
+  },
+  {
+    title: "The Dark Knight Rises",
+    year: 2012,
+    director: "Christopher Nolan",
+    imdbRating: 8.5
+  }
+];
+
+const titles = movies => movies.map(movie => movie.title);
+const byNolan = movie => movie.director === "Christopher Nolan";
+const filter = (movies, func) => movies.filter(func);
+const goodRating = movie => movie.imdbRating >= 7.5;
+const ratings = movies => movies.map(movie => movie.imdbRating);
+const average = array => array.reduce((sum, value) => sum + value, 0) / array.length;
+
+console.log("List of movies ----> " + JSON.stringify(movieList));
+console.log("\nTitles of all movies ----> " + titles(movieList));
+const nolanMovieList = filter(movieList, byNolan);
+console.log("\nTotal number of Nolan movies ----> " + nolanMovieList.length);
+console.log("\nBest movies from the list ----> " + titles(filter(movieList, goodRating)));
+console.log("\nAverage rating of Nolan movies ----> " + average(ratings(nolanMovieList)));
